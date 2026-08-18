0c7d74b244526402956b7b34177f7f1679323de0
Yudhishthir <yudhishthir@revateam.com>
2026-08-18 10:58:08 +0530

    Initial Commit

---
+++ Truedata/TruedataApp.swift
@@ -0,0 +1,17 @@
+//
+//  TruedataApp.swift
+//  Truedata
+//
+//  Created by Devkinandan Dendwal on 18/08/26.
+//
+
+import SwiftUI
+
+@main
+struct TruedataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
